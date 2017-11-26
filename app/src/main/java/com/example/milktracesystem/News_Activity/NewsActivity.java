e5905676bb3e994a4dc01cbb2a63eac153b3cedd
lichang98 <15365811995@163.com>
2017-11-26 15:05:41 +0800

    悬浮按钮效果添加完毕，继续修改查询界面

--- app/src/main/java/com/example/milktracesystem/News_Activity/NewsActivity.java
+++ app/src/main/java/com/example/milktracesystem/News_Activity/NewsActivity.java
@@ -10,8 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.example.milktracesystem.MaterialDesignComps.materialedittext.floatbutton.PromotedActionsLibrary;
 import com.example.milktracesystem.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 
@@ -73,14 +75,36 @@ public class NewsActivity extends AppCompatActivity{
         listView.setOnItemClickListener(new ListItemOnClickListener());
         */
         //悬浮按钮点击
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(NewsActivity.this, "FINDING MORE", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(NewsActivity.this, "FINDING MORE", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.floating_button_frame);//用于存储悬浮按钮的frame
+        PromotedActionsLibrary promotedActionsLibrary = new PromotedActionsLibrary();   //使用库
+        promotedActionsLibrary.setup(getApplicationContext(),frameLayout);
+        //定义悬浮按钮显示的图标以及点击事件
+        promotedActionsLibrary.addItem(getResources().getDrawable(R.drawable.edit),setFloatingButtonListener(0));
+        promotedActionsLibrary.addItem(getResources().getDrawable(R.drawable.send),setFloatingButtonListener(1));
+        promotedActionsLibrary.addItem(getResources().getDrawable(R.drawable.update),setFloatingButtonListener(2));
+        //主按钮
+        promotedActionsLibrary.addMainItem(getResources().getDrawable(R.drawable.open));
+
+
+
     }
+
+    /**
+     * 设置悬浮按钮的点击事件
+     * @param id  点击按钮的位置
+     */
+    public View.OnClickListener setFloatingButtonListener(int id){
+        //TODO
+        return null;
+    }
+
     //ToolBar 菜单
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.general_toolbar,menu);
